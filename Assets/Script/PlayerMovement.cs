197f7d28fe15127de807ceb7e7ad37e5b7b7fd33
SadBread <lukas.cederberg05@gmail.com>
2024-09-23 20:51:53 +0200

    Yippie
    Yippie

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,214 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float jumpForce = 300f;
+    [SerializeField] private Transform leftFoot, rightFoot;
+    [SerializeField] private Transform spawnPosition;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private AudioClip jumpSound, pickupSound;
+
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Image fillColor;
+    [SerializeField] private Color goodHealth, badHealth;
+    [SerializeField] private TMP_Text mushroomText;
+
+    private float horizontalValue;
+    private float rayDistance = 0.25f;
+    private bool isGrounded;
+    private bool canMove;
+    private int startingHealth = 5;
+    private int currentHealth = 0;
+
+    //
+    private int jumpBuffer = 0;
+    private int coyoteTime = 0;
+    private int coyoteWait = 0;
+    //
+
+    private Rigidbody2D rgbd;
+    private SpriteRenderer rend;
+    private Animator anim;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canMove = true;
+        currentHealth = startingHealth;
+        mushroomText.text = "" + Info.mushroomsCollected;
+        rgbd = GetComponent<Rigidbody2D>();
+        rend = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
+        transform.position = spawnPosition.position;
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalValue = Input.GetAxis("Horizontal");
+
+        if (horizontalValue < 0)
+        {
+            FlipSprite(true);
+        }
+
+        if (horizontalValue > 0)
+        {
+            FlipSprite(false);
+        }
+
+        if (coyoteWait > 0) { coyoteWait--; }
+        if (CheckIfGrounded()) { if (coyoteWait == 0) { coyoteTime = 30; } } else { if (coyoteTime > 0) { coyoteTime--; } }
+
+        if ((Input.GetButtonDown("Jump") || jumpBuffer > 0) && (coyoteTime > 0 || CheckIfGrounded()))
+        {
+            Jump();
+        }
+
+        if (jumpBuffer > 0) { --jumpBuffer; }
+        if (Input.GetButtonDown("Jump") && CheckIfGrounded() == false)
+        {
+            jumpBuffer = 50;
+        }
+
+        anim.SetFloat("MoveSpeed", Mathf.Abs(rgbd.velocity.x));
+        anim.SetFloat("VerticalSpeed", rgbd.velocity.y);
+        anim.SetBool("IsGrounded", CheckIfGrounded());
+    }
+
+    private void FixedUpdate()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+
+        rgbd.velocity = new Vector2(horizontalValue * moveSpeed * Time.deltaTime, rgbd.velocity.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Mushroom"))
+        {
+            Destroy(other.gameObject);
+            Info.mushroomsCollected++;
+            mushroomText.text = "" + Info.mushroomsCollected;
+            audioSource.PlayOneShot(pickupSound, 0.05f);
+            audioSource.pitch = Random.Range(0.8f, 1.2f);
+        }
+
+        if (other.CompareTag("Health"))
+        {
+            RestoreHealth(other.gameObject);
+        }
+
+    }
+    private void FlipSprite(bool direction)
+    {
+        rend.flipX = direction;
+    }
+
+    private void Jump()
+    {
+        jumpBuffer = 0;
+        coyoteTime = 0;
+        coyoteWait = 30;
+
+        rgbd.velocity = new Vector2(rgbd.velocity.x, 0);
+        rgbd.AddForce(new Vector2(0, jumpForce));
+        audioSource.PlayOneShot(jumpSound, 0.25f);
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        UpdateHealthBar();
+
+        if (currentHealth <= 0)
+        {
+            Respawn();
+        }
+    }
+
+    public void TakeKnockBack(float knockbackForce, float upwards)
+    {
+        canMove = false;
+        rgbd.AddForce(new Vector2(knockbackForce, upwards));
+        Invoke("CanMoveAgain", 0.25f);
+    }
+
+    private void CanMoveAgain()
+    {
+        canMove = true;
+    }
+    private void Respawn()
+    {
+        currentHealth = startingHealth;
+        UpdateHealthBar();
+        transform.position = spawnPosition.position;
+        rgbd.velocity = Vector2.zero;
+    }
+
+    private void RestoreHealth(GameObject healthPickup)
+    {
+        if (currentHealth >= startingHealth)
+        {
+            return;
+        }
+        else
+        {
+            int healthToRestore = healthPickup.GetComponent<HealthPickups>().healthAmount;
+            currentHealth += healthToRestore;
+            UpdateHealthBar();
+            Destroy(healthPickup);
+
+            if (currentHealth >= startingHealth)
+            {
+                currentHealth = startingHealth;
+            }
+        }
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthSlider.value = currentHealth;
+
+        if (currentHealth >= 2)
+        {
+            fillColor.color = goodHealth;
+        }
+        else
+        {
+            fillColor.color = badHealth;
+        }
+    }
+
+    private bool CheckIfGrounded()
+    {
+        RaycastHit2D leftHit = Physics2D.Raycast(leftFoot.position, Vector2.down, rayDistance, whatIsGround);
+        RaycastHit2D rightHit = Physics2D.Raycast(rightFoot.position, Vector2.down, rayDistance, whatIsGround);
+
+        Debug.DrawRay(leftFoot.position, Vector2.down * rayDistance, Color.blue, 0.25f);
+        Debug.DrawRay(rightFoot.position, Vector2.down * rayDistance, Color.red, 0.25f);
+
+        if (leftHit.collider != null && leftHit.collider.CompareTag("Ground") || rightHit.collider != null && rightHit.collider.CompareTag("Ground"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
