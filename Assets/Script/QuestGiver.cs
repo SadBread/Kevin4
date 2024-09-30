1a6a9653a40fc22c4728f4ee76b2eb800abef6e5
SadBread <lukas.cederberg05@gmail.com>
2024-09-30 15:38:59 +0200

    Line breaks
    Line breaks

--- Assets/Script/QuestGiver.cs
+++ Assets/Script/QuestGiver.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// 
+/// </summary>
 public class QuestGiver : MonoBehaviour
 {
     [SerializeField] private GameObject textPopUp;
